206053f58e614b8f2dbd7f6c94ef878a1a24315c
jlareck <medik2507@gmail.com>
2020-11-10 10:32:14 +0200

    implemented Dekkers lock

---
+++ src/main/java/DekkerLockTest.java
@@ -0,0 +1,42 @@
+
+import task1.DekkerLock;
+
+import java.util.ArrayList;
+
+public class DekkerLockTest {
+
+    public static void main(String[] args) {
+        test(new WithoutLockTestThread());
+        test(new LockTestThread(new DekkerLock()));
+        printResult();
+    }
+
+    private static void test(Runnable runnable) {
+        int numOfThreads = 2;
+        ArrayList<Thread> threads = new ArrayList<>();
+
+        for (int i = 0;  i < numOfThreads; i++) {
+            threads.add(new Thread(runnable));
+        }
+
+        for(Thread thread: threads) {
+            thread.start();
+        }
+
+        for(Thread thread: threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException x) {
+                x.printStackTrace();
+            }
+        }
+
+
+    }
+    private static void printResult() {
+        System.out.println("----------------------------------------------------");
+        System.out.println(("Counter with Lock: " + LockTestThread.value));
+        System.out.println(("Counter without Lock: " + WithoutLockTestThread.counter));
+        System.out.println("----------------------------------------------------");
+    }
+}
---
+++ src/main/java/WithoutLockTestThread.java
@@ -0,0 +1,19 @@
+public class WithoutLockTestThread implements Runnable {
+    public static int counter = 0;
+
+    public WithoutLockTestThread() {
+        System.out.println("Creating new thread...");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread without lock: " + Thread.currentThread().getName());
+        for (int i = 0; i < 100000; i++) {
+            f();
+        }
+    }
+
+    private void f() {
+        counter++;
+    }
+}
