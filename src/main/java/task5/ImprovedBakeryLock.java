55989e83aa7bc2aa48fe862f41401cb908c51e44
DarynaBond <35077916+WAG13@users.noreply.github.com>
2020-11-16 00:56:13 +0200

    added ImprovedBakeryLockTest

--- src/main/java/task5/ImprovedBakeryLock.java
+++ src/main/java/task5/ImprovedBakeryLock.java
@@ -8,17 +8,18 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
 public class ImprovedBakeryLock extends AbstractFixnumLock {
 
     private static AtomicInteger currTicket;
+    private static final int MAX = Integer.MAX_VALUE;
     private AtomicIntegerArray tickets;
 
     public ImprovedBakeryLock(int maxNumberOfThreads) {
         super(maxNumberOfThreads);
         tickets = new AtomicIntegerArray(maxNumberOfThreads);
-        currTicket = new AtomicInteger(0);
+        currTicket = new AtomicInteger(1);
     }
 
     @Override
     public void lock() {
-        tickets.set(getId(), currTicket.incrementAndGet());
+        tickets.set(getId(), getAndChange());
 
         for (int i = 0; i < tickets.length(); ++i){
             if (i != getId()){
@@ -30,7 +31,25 @@ public class ImprovedBakeryLock extends AbstractFixnumLock {
 
     }
 
+    private int getAndChange() {
+        while (true) {
+            int ticket = currTicket.get();
+            if (ticket == MAX) {
+                if (currTicket.compareAndSet(MAX, 1)) {
+                    return MAX;
+                } else {
+                    continue;
+                }
+            }
+            if (currTicket.compareAndSet(ticket, ticket + 1)) {
+                return ticket;
+            }
+        }
+    }
+
     @Override
-    public void unlock() { tickets.set(getId(), 0); }
+    public void unlock() {
+        tickets.set(getId(), 0);
+    }
 
 }
